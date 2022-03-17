18023dcf8e415ceb6389db10a34bf642d012d880
Sharjil <asharjil@deloitte.com@users.noreply.github.com>
2022-03-17 10:22:47 +0530

    Array and Strings 1

---
+++ src/Array_And_Strings_Assignment/SubArraySum.java
@@ -0,0 +1,27 @@
+package Array_And_Strings_Assignment;
+
+import java.util.Scanner;
+
+public class SubArraySum {
+    public void  main(String []args)
+    {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int count=0,sum=0;
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++) {
+            sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += a[j];
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+}
