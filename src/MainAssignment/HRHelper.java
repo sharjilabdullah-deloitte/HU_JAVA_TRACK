ecd1b50417b30bbe94e8af259bada7bdb388941a
Sharjil <asharjil@deloitte.com@users.noreply.github.com>
2022-03-21 23:19:14 +0530

    Connection of IDE with JDBC

--- src/MainAssignment/HRHelper.java
+++ src/MainAssignment/HRHelper.java
@@ -1,5 +1,26 @@
 package MainAssignment;
 
-public class HRHelper {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class HRHelper {
+    public static void main(String args[]) {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp-records", "root", "password");
+            Statement statement= connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from employee");
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("name"));
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
